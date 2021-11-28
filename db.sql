f95c391d607380e4183efee26c91f20728a35886
l33gun4 <dlrjsdk105@gmail.com>
2021-11-28 16:29:06 +0900

    [토요일]헤더 로그인

--- db.sql
+++ db.sql
@@ -44,8 +44,6 @@ CREATE TABLE user(
 );
 INSERT INTO user(oid, id, password, name, birthday, email, gender, phone, type, image_url, home, google_id) VALUES(0, 'admin', 'd82494f05d6917ba02f7aaa29689ccb444bb73f20380876cb05d1f37537b7892','관리자','2021-11-10','admin@email.com','무','010-0000-0000','전체관리자', '', '','118011962661211748557');
 
-INSERT INTO user(oid, id, password, name, birthday, email, gender, phone, type, image_url, home, google_id) VALUES(0, 'admin', 'd82494f05d6917ba02f7aaa29689ccb444bb73f20380876cb05d1f37537b7892','관리자','2021-11-10','admin@email.com','무','010-0000-0000','전체관리자', 'https://lh3.googleusercontent.com/a/AATXAJxgP6p-_2wssUEVCxgDX5oukWJyuuHUQ4GL2MUC=s96-c', '','118011962661211748557');
-
 CREATE TABLE type (
                      `name` VARCHAR (10) NOT NULL,
                      `level` INT(2) NOT NULL
